6a3e590ac29931effe43482c4752f07c603e605a
Nikita Nechepurenko <rufflerock@gmail.com>
2023-03-12 13:42:13 +0100

    extract main session participants list into a property and push artifacts to s3 in the build script

--- backend/src/main/java/io/nechn/lcct/controller/ChallengeController.java
+++ backend/src/main/java/io/nechn/lcct/controller/ChallengeController.java
@@ -2,10 +2,12 @@ package io.nechn.lcct.controller;
 
 import io.nechn.lcct.model.ChallengeStatusResponse;
 import io.nechn.lcct.service.ChallengeService;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class ChallengeController {
+
+    @Value("${session.main.participants}")
+    private String mainSessionParticipantsListString;
 
     private final ChallengeService challengeService;
 
@@ -26,8 +31,8 @@ public class ChallengeController {
 
     @GetMapping("/getChallengeStatus/session/{session}")
     public ResponseEntity<List<ChallengeStatusResponse>> getSessionChallengeStatusList(@PathVariable String session) {
-        final var leetCodeIdList = List.of("nechn", "nek1tko");
-        final var sessionResponse = leetCodeIdList.stream()
+        final var leetCodeIdList = mainSessionParticipantsListString.split(",");
+        final var sessionResponse = Arrays.stream(leetCodeIdList)
                                                   .map(challengeService::getStatusResponse)
                                                   .map(Optional::get)
                                                   .collect(Collectors.toList());
--- build/build_project.sh
+++ build/build_project.sh
@@ -10,5 +10,8 @@ mvn clean package
 cd ..
 cp -r t_backend/target/*.jar .
 
+aws s3 cp lc-challenge-tracker-backend-0.0.1-SNAPSHOT.jar s3://nechn-leetcode-challenge-deploy/lc-challenge-tracker-backend-0.0.1-SNAPSHOT.jar
+aws s3 cp ../py-graphql-proxy/main.py s3://nechn-leetcode-challenge-deploy/py-graphql-proxy/main.py
+
 rm -r ../frontend/build
 rm -r t_backend
